bb9354e13fabb6fe981ffb2a9a793a47c3d75757
Ayushi <ayushi27@uab.edu>
2017-10-18 20:05:28 -0500

    Welcome Django page loaded!

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ayushi\desktop\cscour~1\projects\sidepr~1\apps\todolist\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
